9cb8380e569136bba0c565343705d97ae212bc56
kuntus1 <vlasenko.max@lll.kpi.ua>
2023-01-21 15:35:50 +0200

    checkout update for laptop

--- 2-0-data-structures-and-algorithms/2-2-3-linked-queue/src/main/java/com/bobocode/cs/LinkedQueue.java
+++ 2-0-data-structures-and-algorithms/2-2-3-linked-queue/src/main/java/com/bobocode/cs/LinkedQueue.java
@@ -14,6 +14,7 @@ import com.bobocode.util.ExerciseNotCompletedException;
  * @author Taras Boychuk
  * @author Ivan Virchenko
  */
+// Check for update on laptop
 public class LinkedQueue<T> implements Queue<T> {
 
     private int size;
